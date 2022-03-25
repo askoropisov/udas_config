cab167e1e8254d4e847188f1f36f1569553c865e
Артем Cкорописов <sckoropisow.ar@yandex.ru>
2022-03-25 13:49:36 +0300

    исправлен формат

--- src/redux/configuration/EScaleConfigSlice.js
+++ src/redux/configuration/EScaleConfigSlice.js
@@ -5,7 +5,11 @@ const init = {
     Peak356: 0,
     Peak81: 0,
     Range: 0,
-    Duration: 0,
+    Duration: {
+        hours: "00",
+        minutes: "00",
+        seconds: "00"
+    },
     S0: 0,
     S1: 0.5,
     S2: 0
@@ -27,11 +31,11 @@ export const EScaleSlice = createSlice({
             state.Peak81 = data.peak81
             state.Range = data.range
             state.Duration = data.duration
-            state.S0=data.s0
-            state.S1=data.s1
-            state.S2=data.s2
+            state.S0 = data.s0
+            state.S1 = data.s1
+            state.S2 = data.s2
         })
-        builder.addCase(getEscale.rejected, (state, action) =>{
+        builder.addCase(getEscale.rejected, (state, action) => {
             console.log(action)
         })
 
