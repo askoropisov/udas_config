1088113025c856c38e55c28d8a0fa4383eeeda46
Артем Cкорописов <sckoropisow.ar@yandex.ru>
2022-05-17 11:35:38 +0300

    замена порта

--- src/utils/axios/configureAxios.js
+++ src/utils/axios/configureAxios.js
@@ -5,7 +5,7 @@ import axios from "axios"
 const configureAxios = () => {
     axios.interceptors.request.use(config => {
         if (process.env.NODE_ENV === "development") {
-            config.url = "https://localhost:5001" + config.url
+            config.url = "https://localhost:6001" + config.url
         }
         return config
     })
